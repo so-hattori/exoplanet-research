40226531244298c00ac13efea2504a5156660076
so-hattori <soichiro@nyu.edu>
2014-06-19 15:59:51 -0400

    update chi_test with functions

--- chi_test_functions.py
+++ chi_test_functions.py
@@ -40,8 +40,8 @@ length = clean_flux.shape[0]
 period_time = np.linspace(first, last, 44100) - first
 
 #define arguments for the box_model
-period = np.arange(0, 4939)
-phase = np.arange(0, 2529)
+period = np.arange(0, 4935)
+phase = np.arange(0, 2521)
 depth = 0.006
 width = np.arange(0, 270)
 
@@ -52,21 +52,71 @@ depth_chi_value_list = []
 width_chi_value_list = []
 
 period_interval = np.arange(4900, 5000)
-phase_interval = np.arange(2500, 2600)
-depth_interval = np.arange(0.005, 0.020, 0.001)
+phase_interval = np.arange(2400, 2800)
+depth_interval = np.arange(-0.020, 0.020, 0.001)
 width_interval = np.arange(250, 500)
 
-period_chi_dict = f.period_search(period_chi_value_list, period_interval, phase, depth, width, length, clean_flux)
-period_best_fit = f.best_parameter(period_chi_dict, period_chi_value_list)
-print period_best_fit
-print period_time[period_best_fit]
+#Used in plotting the x label
+x_tick = []
+xlab2 = 0
+
+#Refactoring to if/elif for the 4 parameters. This part should be heavily optimized later.
+choose_parameter = raw_input('Period[1], Phase[2], Depth[3], Width[4]: ')
+if choose_parameter == '1':
+	#Period 
+	period_chi_dict = f.period_search(period_chi_value_list, period_interval, phase, depth, width, length, clean_flux)
+	period_best_fit = f.best_parameter(period_chi_dict, period_chi_value_list)
+	print 'Best fit data point-wise: ', period_best_fit
+	print 'Best fit in days: ', period_time[period_best_fit]
+	period = np.arange(0, period_best_fit)
+	chi_value_list = period_chi_value_list
+	for i in period_interval:
+		x_tick.append(period_time[i])
+	xlab2 = 'Period (days)'
+
+elif choose_parameter == '2':
+	#Phase
+	phase_chi_dict = f.phase_search(phase_chi_value_list, phase_interval, phase, depth, width, length, clean_flux)
+	phase_best_fit = f.best_parameter(phase_chi_dict, phase_chi_value_list)
+	print 'Best fit data point-wise: ', phase_best_fit
+	print 'Best fit in days:', period_time[phase_best_fit]
+	phase = np.arange(0, phase_best_fit)
+	chi_value_list = phase_chi_value_list
+	for i in phase_interval:
+		x_tick.append(period_time[i])
+	xlab2 = 'Phase (days)'
+	
+elif choose_parameter == '3':
+	#Depth
+	depth_chi_dict = f.depth_search(depth_chi_value_list, depth_interval, period, phase, width, length, clean_flux)
+	depth_best_fit = f.best_parameter(depth_chi_dict, depth_chi_value_list)
+	print depth_best_fit
+	depth = depth_best_fit
+	chi_value_list = depth_chi_value_list
+	for i in depth_interval:
+		x_tick.append(i)
+	xlab2 = 'Depth (unitless)'
+elif choose_parameter == '4':
+	#Width
+	width_chi_dict = f.width_search(width_chi_value_list, width_interval, period, phase, depth, length, clean_flux)
+	width_best_fit = f.best_parameter(width_chi_dict, width_chi_value_list)
+	print width_best_fit
+	print period_time[width_best_fit]
+	width = np.arange(0, width_best_fit)
+	chi_value_list = width_chi_value_list
+	for i in width_interval:
+		x_tick.append(period_time[i])
+	xlab2 = 'Width (days)'
 
 #Once the best fit parameter is found, update the parameters
-period = np.arange(0, period_best_fit)
+# period = np.arange(0, period_best_fit)
+# phase = np.arange(0, phase_best_fit)
+# depth = depth_best_fit
+# width = np.arange(0, width_best_fit)
 
-period_in_days = []
-for i in period_interval:
-	period_in_days.append(period_time[i])
+# period_in_days = []
+# for i in period_interval:
+# 	period_in_days.append(period_time[i])
 
 
 fig1 = plt.figure()
@@ -83,9 +133,10 @@ plottitle="Light Curve for %s"%obsobject
 sub1.set_title(plottitle)
 
 sub2 = fig1.add_subplot(122)
-sub2.plot(period_in_days, period_chi_value_list, color = 'black', marker = '.', markersize = 10)
+#Refactor the second argument of the following line so that it simplifies.
+sub2.plot(x_tick, chi_value_list, color = 'black', marker = '.', markersize = 10)
 sub2.ticklabel_format(useOffset = False)
-xlab2 = 'Period (days)'
+# xlab2 = 'Period (days)'
 sub2.set_xlabel(xlab2)
 ylab = r'$\chi^2$'
 sub2.set_ylabel(ylab)
