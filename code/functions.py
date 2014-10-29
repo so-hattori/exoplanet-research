1ce45823c26b2ea5c38f790da16c9bf0d618d954
so-hattori <soichiro@nyu.edu>
2014-10-29 16:40:57 -0400

    remove deprecated functions

--- code/functions.py
+++ code/functions.py
@@ -12,82 +12,23 @@ def return_arrays(kplr_id, filter_size):
 	client = kplr.API()
 	star = client.star(kplr_id)
 	lcs = star.get_light_curves(short_cadence=False)
-	#Figure out the number of data sets so that the size of the
-	#array can be set.
-	size = len(lcs)
-	time_array = np.zeros(size)
-	flux_array = np.zeros(size)
-	ferr_array = np.zeros(size)
-	var_array = np.zeros(size)
-	med_flux_array = np.zeros(size)
-	#To use in indexing the array
-	i = 0
+	time_list, flux_list, med_flux_list, var_list, ferr_list = [], [], [], [], []
 	for lc in lcs:
 		with lc.open() as f:
 			hdu_data = f[1].data
 			time, flux, ferr = fix_data(hdu_data)
-			time_array[i] = time
-			flux_array[i] = flux
+			time_list.append(time)
+			flux_list.append(flux)
 			median = median_filter(flux, filter_size)
-			ferr_array[i] = ferr/median
-			var_array[i] = (ferr/median)**2
-			med_flux_array[i] = flux/median
-		i += 1
-	time = np.concatenate(time_array)
-	flux = np.concatenate(flux_array)
-	med_flux = np.concatenate(med_flux_array)
-	variance = np.concatenate(var_array)
-	ferr = np.concatenate(ferr_array)
+			ferr_list.append(ferr/median)
+			var_list.append((ferr / median)**2)
+			med_flux_list.append(flux / median)
+			time = np.concatenate(time_list)
+	flux = np.concatenate(flux_list)
+	med_flux = np.concatenate(med_flux_list)
+	variance = np.concatenate(var_list)
+	ferr = np.concatenate(ferr_list)
 	return time, flux, med_flux, variance, ferr
-
-
-#function to open the FITS format file given kepler id and filename.
-def openfile(kplr_id, kplr_file):
-	path = '/Users/SHattori/.kplr/data/lightcurves/%s' %kplr_id
-	os.chdir(path)
-
-	FITSfile = pyfits.open(kplr_file)
-	dataheader = FITSfile[1].header
-	topheader = FITSfile[0].header
-	jdadj = str(dataheader['BJDREFI']) # the part that needs to be subtracted for julian date
-	obsobject = str(dataheader['OBJECT']) # the ID of the observed object
-	lightdata = FITSfile[1].data #the part of the FITS file where all the data is stored.
-	FITSfile.close()	
-	#Choose the data you want returned by commenting out either of the next two lines.
-	return jdadj, obsobject, lightdata
-	# return lightdata
-
-def comb_openfile(kplr_id, kplr_filenamelist):
-	path = '/Users/SHattori/.kplr/data/lightcurves/%s' %kplr_id
-	os.chdir(path)
-
-	lightdata_list = []
-	for kplr_file in kplr_filenamelist:
-		FITSfile = pyfits.open(kplr_file)
-		dataheader = FITSfile[1].header
-		topheader = FITSfile[0].header
-		jdadj = str(dataheader['BJDREFI']) # the part that needs to be subtracted for julian date
-		obsobject = str(dataheader['OBJECT']) # the ID of the observed object
-		lightdata = FITSfile[1].data #the part of the FITS file where all the data is stored.
-		lightdata_list.append(lightdata)
-		FITSfile.close()
-	return lightdata_list
-
-#Create an optimized version of obtaining kplr data with only the ID.
-def kplr_list(kplr_id):
-	client = kplr.API()
-	star = client.star(kplr_id)
-	lcs = star.get_lightcurves(short_cadence = False)
-	time, flux, flux_err = [], [], []
-	for lc in lcs:
-		with lc.open() as f:
-		# The lightcurve data are in the first FITS HDU.
-			hdu_data = f[1].data
-			time.append(hdu_data["time"])
-			flux.append(hdu_data["pdcsap_flux"])
-			flux_err.append(hdu_data["pdcsap_flux_err"])
-	return time, flux, flux_err
-
 
 
 #function to handle removing Nan values in the arrays
@@ -100,61 +41,6 @@ def fix_data(lightdata):
 	time = time[m]
 	flux_err = flux_err[m]
 	return time, flux, flux_err
-
-def remove_nan(time, flux, ferr):
-	m = np.isfinite(time) * np.isfinite(flux) * np.isfinite(ferr)
-	flux = flux[m]
-	time = time[m]
-	ferr = ferr[m]
-	return time, flux, ferr
-
-def comb_data(lightdata_list):
-	time_list = []
-	flux_list = []
-	variance_list = []
-	for lightdata in lightdata_list:
-		time, flux, flux_err = fix_data(lightdata)
-		#rescale the flux and convert to flux_err
-		flux, variance = rescale(flux, flux_err)
-		time_list.append(time)
-		flux_list.append(flux)
-		variance_list.append(variance)
-	comb_time = np.concatenate(time_list)
-	comb_flux = np.concatenate(flux_list)
-	comb_variance = np.concatenate(variance_list)
-	return comb_time, comb_flux, comb_variance
-
-#Create a function that combines the data without normalization.
-def nonnorm_data(lightdata_list):
-	time_list = []
-	flux_list = []
-	variance_list = []
-	for lightdata in lightdata_list:
-		time, flux, flux_err = fix_data(lightdata)
-		#rescale the flux and convert to flux_err
-		# flux, variance = rescale(flux, flux_err)
-		time_list.append(time)
-		flux_list.append(flux)
-		variance_list.append((flux_err**2))
-	comb_time = np.concatenate(time_list)
-	comb_flux = np.concatenate(flux_list)
-	comb_variance = np.concatenate(variance_list)
-	return comb_time, comb_flux, comb_variance
-
-
-#Rescale the flux and the error.
-#The 1 sigma error given by the kepler data is converted to variance.
-def rescale(flux, flux_err):
-	median = np.median(flux)
-	scaled_flux = (flux / median)
-	scaled_variance = (flux_err / median) ** 2
-	return scaled_flux, scaled_variance
-
-def unity_normalize(flux, flux_err):
-	median = np.median(flux)
-	unity_flux = flux / median
-	scaled_variance = (flux_err / median) ** 2
-	return unity_flux, scaled_variance
 
 #Document later
 def box(period, offset, depth, width, time):
@@ -192,6 +78,7 @@ def get_depth_and_ln_like(data_array, offset, width, time, inverse_variance):
 	depth_variance = 0
 	if data_in_transit.shape[0] == 0:
 		depth = 0.0
+		value = 1.0
 	else:
 		#If coincedentally the mean inside the window is
 		#larger than 1.0, we need to assign the depth to be 0.
@@ -249,22 +136,6 @@ def raw_injection(period, offset, depth, width, time, flux):
 def ln_like(data_array, model_array, inv_variance):
 	chi2 = ((data_array - model_array)**2)*(inv_variance)
 	return (-1/2)*np.sum(chi2)
-
-#Create a test function to return a relative ln_like without 
-#taking into account the variance.
-def pre_ln_like(data_array, model_array):
-	chi2 = (data_array - model_array)**2
-	return (-1/2)*np.sum(chi2)
-
-
-#returns the sum of the chi_squared values
-def sum_chi_squared(data_array, model_array, variance):
-	chi2_array = ((data_array - model_array)**2) / (variance) 
-	return np.sum(chi2_array)
-
-def pre_sum(data_array, model_array):
-	chi2_array = (data_array - model_array) **2
-	return np.sum(chi2_array)
 
 #generate vertical lines
 def vertical_lines(inj_period, time, plot_designation, inj_width, ylim_limits):
--- code/kplr_getlist.py
+++ code/kplr_getlist.py
@@ -6,9 +6,11 @@ mpl.rcParams['figure.figsize'] = 12,9
 plt = mpl.pyplot
 import numpy as np
 import functions
+import time as timer
 client = kplr.API()
 
-star = client.star(8800954)
+star = client.star(2162635)
+width = 1.1
 
 lcs = star.get_light_curves(short_cadence=False)
 
@@ -16,6 +18,7 @@ med_flux_list = []
 filter_size = 80
 
 time_list, flux_list, var_list, ferr_list = [], [], [], []
+start_time = timer.time()
 for lc in lcs:
     with lc.open() as f:
         # The lightcurve data are in the first FITS HDU.
@@ -34,27 +37,41 @@ med_flux = np.concatenate(med_flux_list)
 variance = np.concatenate(var_list)
 ferr = np.concatenate(ferr_list)
 
-#depth = 0.003
-width = 0.8
+# time, flux, med_flux, variance, ferr = functions.return_arrays(2162635, 80)
+width = 1.1
+print "Loading data", timer.time() - start_time
 
 #Run the search
-main_array = np.asarray([functions.get_depth_and_ln_like(med_flux, o, width, time, 1/variance) for o in time])
+#time_grid makes the time array coarser. This will allow the number of searches to
+#be much less than doing the calculations at all times.
+time_grid = np.arange(min(time), max(time), width/10)
+print time.shape, time_grid.shape
+start_time = timer.time()
+main_array = np.asarray([functions.get_depth_and_ln_like(med_flux, o, width, time, 1/variance) for o in time_grid])
+print "Search time", timer.time() - start_time
 depth_array = main_array[:,0]
 depth_variance_array = main_array[:,1]
 ln_like_array = main_array[:,2]
 
 #Find the peaks of the ln_like array
 index_max_like = np.argmax(ln_like_array)
-time_at_like_max = time[index_max_like]
+time_at_like_max = time_grid[index_max_like]
 print time_at_like_max
 lt = time < (time_at_like_max+width)
 ut = (time_at_like_max-width) < time
-
 window_time = lt*ut
 w_time = time[window_time]
 window_flux = med_flux[window_time]
-window_ln_like = ln_like_array[window_time]
 window_error = ferr[window_time]
+
+
+lt = time_grid < (time_at_like_max+width)
+ut = (time_at_like_max-width) < time_grid
+window_time = lt*ut
+w_time_grid = time_grid[window_time]
+
+window_ln_like = ln_like_array[window_time]
+
 
 fig1 = plt.figure()
 # sub1 = fig1.add_subplot(211)
@@ -74,7 +91,7 @@ sub1.set_ylabel("Median-filtered Flux")
 sub1.grid()
 
 sub2 = fig1.add_subplot(212)
-sub2.plot(time, ln_like_array, '.b', markersize = 2)
+sub2.plot(time_grid, ln_like_array, '.b', markersize = 2)
 sub2.set_xlabel("Days")
 sub2.set_ylabel("Ln Likelihood")
 sub2.grid()
@@ -85,18 +102,20 @@ sub3 = fig2.add_subplot(211)
 sub3.errorbar(w_time, window_flux, yerr=window_error, fmt = '.')
 sub3.set_xlabel("Days")
 sub3.set_ylabel("Median-filtered Flux")
+sub3.set_xlim([time_at_like_max-width, time_at_like_max+width])
 sub3.grid()
 
 sub4 = fig2.add_subplot(212)
 # sub4.plot(time, ln_like_array, '.b')
-sub4.plot(w_time, window_ln_like, '.b')
+sub4.plot(w_time_grid, window_ln_like, '.b')
 sub4.set_xlabel("Days")
 sub4.set_ylabel("Ln_Like")
+sub4.set_xlim([time_at_like_max-width, time_at_like_max+width])
 sub4.grid()
 
 fig3 = plt.figure()
 sub5 = fig3.add_subplot(211)
-sub5.plot(time, depth_array, '.b', markersize=2)
+sub5.plot(time_grid, depth_array, '.b', markersize=2)
 
 plt.show()
 
